9940831de47cdb5c3e74b899f5d459d2ddc07a3e
Andrea Grandi <a.grandi@gmail.com>
2021-04-03 11:43:02 +0200

    Add protein-translation solution to Go track

---
+++ go/protein-translation/protein_translation.go
@@ -0,0 +1,59 @@
+package protein
+
+import (
+	"errors"
+)
+
+var Codons = map[string]string{
+	"AUG": "Methionine",
+	"UUU": "Phenylalanine",
+	"UUC": "Phenylalanine",
+	"UUA": "Leucine",
+	"UUG": "Leucine",
+	"UCU": "Serine",
+	"UCC": "Serine",
+	"UCA": "Serine",
+	"UCG": "Serine",
+	"UAU": "Tyrosine",
+	"UAC": "Tyrosine",
+	"UGU": "Cysteine",
+	"UGC": "Cysteine",
+	"UGG": "Tryptophan",
+	"UAA": "STOP",
+	"UAG": "STOP",
+	"UGA": "STOP",
+}
+
+var (
+	ErrStop        = errors.New("found STOP sequence")
+	ErrInvalidBase = errors.New("invalid sequence")
+)
+
+func FromCodon(input string) (string, error) {
+	if value, ok := Codons[input]; ok {
+		if value == "STOP" {
+			return "", ErrStop
+		} else {
+			return value, nil
+		}
+	}
+	return "", ErrInvalidBase
+}
+
+func FromRNA(input string) ([]string, error) {
+	sequence := []string{}
+	for i := 0; i <= len(input)-3; i += 3 {
+		protein, err := FromCodon(input[i : i+3])
+
+		if err == ErrStop {
+			return sequence, nil
+		}
+
+		if err == ErrInvalidBase {
+			return sequence, err
+		}
+
+		sequence = append(sequence, protein)
+	}
+	return sequence, nil
+}
