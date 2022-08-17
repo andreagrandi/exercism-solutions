9371b11f17067e13ce68aad33d1e21e76b4cd12b
Andrea Grandi <a.grandi@gmail.com>
2022-08-17 11:21:48 +0200

    Update nucleotide-count solution in Go track

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -7,13 +7,11 @@ import "errors"
 type Histogram map[rune]int
 
 // DNA is a list of nucleotides. Choose a suitable data type.
-type DNA struct {
-	strand string
-}
+type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
-///
+// /
 // Counts is a method on the DNA type. A method is a function with a special receiver argument.
 // The receiver appears in its own argument list between the func keyword and the method name.
 // Here, the Counts method has a receiver of type DNA named d.
@@ -25,7 +23,7 @@ func (d DNA) Counts() (Histogram, error) {
 		'T': 0,
 	}
 
-	for _, s := range d.strand {
+	for _, s := range d {
 		switch s {
 		case 'A':
 			h['A'] += 1
